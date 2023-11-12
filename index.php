bf2877ace9ff2dd0ab16c1113c8b545e38ab77a8
kittichok555 <130206989+kittichok555@users.noreply.github.com>
2023-11-12 20:33:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php 
-echo 'hallo';
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- -------------------- head start -------------------- -->
+    <head>
+        <title>kittichok</title>
+    </head>
+    
+    <body>       
+        hallo+++
+    </body>
+</html>
